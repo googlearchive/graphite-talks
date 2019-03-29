a8a54d12f3f339838f11cf0258a8dcbd564d3924
Matthew Broomfield <mattysweeps@google.com>
2019-03-29 09:08:26 -0700

    Add hello world app

---
+++ cf-na-summit-2019_cf-on-k8s/hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
